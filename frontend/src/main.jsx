858bb9389b089355e404f73235f83707a8cc018a
javier <javiemex@yahoo.es>
2026-03-02 10:34:02 +0100

    instalamos React Router y refactorizamos main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>,
 )
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <>
+      <h1>Hola Dashboard</h1>
+    </>
+  )
+}
+
+export default Dashboard
