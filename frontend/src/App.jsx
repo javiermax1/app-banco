38c8ab0cd3c26af271165878ef9331b9e8487193
javier <javiemex@yahoo.es>
2026-03-02 09:59:39 +0100

    vacio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import './App.css';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <h1>holamundo</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
